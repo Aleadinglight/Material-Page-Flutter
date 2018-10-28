d0bb0dccfb4da06bacb9f56268abb95e5d0ab032
unknown <huutam.ptnk@gmail.com>
2018-10-28 18:08:37 +0200

    added pages.dart

--- lib/main.dart
+++ lib/main.dart
@@ -60,24 +60,33 @@ class _MyHomePageState extends State<MyHomePage> {
             child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                new Image.asset(
-                  'assets/hotel.png',
-                  width: 200.0,
-                  height: 200.0,
-                ),
-                new Text(
-                  'Hotels',
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 34.0,
+                new Padding(
+                  padding: new EdgeInsets.only(bottom: 25.0),
+                  child: new Image.asset(
+                    'assets/hotel.png',
+                    width: 200.0,
+                    height: 200.0,
                   ),
                 ),
-                new Text(
-                  'This is the body',
-                  textAlign: TextAlign.center,
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
+                new Padding(
+                  padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: new Text(
+                    'Hotels',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 34.0,
+                    ),
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.only(bottom: 75.0),
+                  child: new Text(
+                    'This is the body',
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
                   ),
                 ),
               ],
